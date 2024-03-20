96f1a05a41dbe284bf461c6b5835474b10a90b35
solovey <ilyasolovey7@gmail.com>
2024-03-20 22:49:59 +0300

    Social media section

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ document.querySelectorAll('.nav-link').forEach((element) => {
         event.srcElement.classList.add('active');
     };
 });
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('slide');
+        }
+    });
+}
+  
+let options = {
+    threshold: [0.5] 
+};
+
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.tgk, .tt, .inst, .youtube');
+  
+for (let elm of elements) {
+    observer.observe(elm);
+}
